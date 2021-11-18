f22a2910ff7762c40c767b53d67dc2e60945823c
GaryFernanYM <miraclevann224@gmail.com>
2021-11-18 11:27:27 -0600

    Examen2

---
+++ ExamenGFYM/src/main/java/pe/edu/upeu/gfym/App.java
@@ -0,0 +1,20 @@
+package pe.edu.upeu.gfym;
+
+import pe.edu.upeu.gfym.examen.ResolucionExamen;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    public static void main( String[] args ){
+    ResolucionExamen Obj=new ResolucionExamen();
+    
+      //Obj.impuestoAutos();
+     //licar1hasta20();
+     //Obj.numerosperfectos();
+     System.out.println(Obj.potencia(3, 5));
+     
+    }
+}
